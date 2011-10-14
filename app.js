2091212b46ea08519dd4650d51e176b7159674d1
Jesper Josefsson <jesper.josefsson@gmail.com>
2011-10-15 01:04:09 +0200

    Added validation function, some rudimentary tests

--- app.js
+++ app.js
@@ -5,13 +5,14 @@
 
 var express = require('express');
 require('express-namespace');
-var bs = require('nodestalker/lib/beanstalk_client');
-var client = bs.Client();
-var tube = 'tube';
-var app = module.exports = express.createServer();
-// nodestalker config
+var bs        = require('nodestalker/lib/beanstalk_client');
+var client    = bs.Client();
+var tube      = 'tube';
+var app       = module.exports = express.createServer();
 
 
+// Validator configuration
+var check = require('validator').check;
 
 // Configuration
 
@@ -37,6 +38,7 @@ app.configure('production', function(){
 app.namespace('/:api_key/providers', function () {
   app.namespace('/:provider_id', function() {
     app.namespace('/journeys', function() {
+      
       /*
        *  /:api_key/providers/:provider_id/journeys
        */
@@ -52,7 +54,19 @@ app.namespace('/:api_key/providers', function () {
         console.log("Got a put request for provider id: " 
         + req.params.provider_id 
         + " and journey id: " 
-        + req.params.journey_id);
+        + req.params.journey_id
+        + " and body: ", req.body);
+        
+        var data = req.body;
+        data.journey_id = req.params.journey_id;
+        data.provider_id = req.params.provider_id;
+        
+        result = app.validate(data);
+        if (!result.valid) {
+          res.send("Not valid :( " + result.message);
+        } else {
+          res.send("Valid!");
+        }
       });
     });
     
@@ -75,7 +89,8 @@ app.namespace('/:api_key/providers', function () {
   });
 });
 
-app.get('/get', function(req, res) {
+app.get('/pop', function(req, res) {
+  console.log("Pop!");
   client.watch(tube).onSuccess(function(data) {
     client.reserve().onSuccess(function(job) {
       console.log(job);
@@ -83,8 +98,8 @@ app.get('/get', function(req, res) {
   });
 });
 
-app.get('/put', function(req, res){
-  console.log("Put!");
+app.get('/push', function(req, res){
+  console.log("Push!");
   client.use(tube).onSuccess(function(data) {
     console.log(data);
 
@@ -94,6 +109,29 @@ app.get('/put', function(req, res){
     });
   });
 });
+
+// Validation
+
+app.validate = function(data) {
+  var acceptedEvents = 
+  [
+    "did_leave_station",
+    "update",
+    "alert"  
+  ];
+  try {
+    check(data.event).notNull().in(acceptedEvents);
+    check(data.id).notNull().notEmpty().isAlphanumeric();
+    check(data.origin_station).isInt().min(1);
+    // check(data.destination_station).isInt().min(1);
+    // check(data.arrival_time).isInt();
+    // check(data.provider_id).isInt().min(1);
+    // check(data.line_id).isInt().min(1);
+  } catch (e) {
+    return {valid: false, message: e.message};
+  }
+  return {valid: true};
+};
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
