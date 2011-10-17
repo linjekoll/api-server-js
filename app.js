0d793391698f6e2be2cf5f71d9f5f72630f6bad3
Linus Oleander <linus@oleander.nu>
2011-10-17 19:24:46 +0200

    Implementing initialize lib

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-/**
-* Module dependencies.
-*/
-
-var express   = require('express'); require('express-namespace');
-var app       = module.exports = express.createServer();
-var validate  = require("./lib/validation.js");
-var helper    = require("./lib/helper.js");
-var queue     = require("./lib/queue.js");
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
+var validate   = require("./lib/validation.js");
+var helper     = require("./lib/helper.js");
+var queue      = require("./lib/queue.js");
+var app        = require("./lib/initialize.js").initialize();
 
 app.namespace('/:api_key/providers', function () {
   app.namespace('/:provider_id', function() {
@@ -35,20 +11,13 @@ app.namespace('/:api_key/providers', function () {
        *  /:api_key/providers/:provider_id/journeys
        */
       app.get('/', function(req, res) {
-        console.log("Got a journey listing request for provider id: " 
-        + req.params.provider_id);
+        console.log("Got a journey listing request for provider id: " + req.params.provider_id);
       });
       
       /*
        *  /:api_key/providers/:provider_id/journeys/:journey_id
        */
       app.put('/:journey_id', function(req, res) {
-        console.log("Got a put request for provider id: " 
-        + req.params.provider_id 
-        + " and journey id: " 
-        + req.params.journey_id
-        + " and body: ", req.body);
-        
         var data = req.body;
         data.journey_id = req.params.journey_id;
         data.provider_id = req.params.provider_id;
@@ -78,27 +47,6 @@ app.namespace('/:api_key/providers', function () {
   */
   app.get('/', function(req, res) {
     console.log("Requested provider listing - route: GET /" + req.params.api_key + "/providers");
-  });
-});
-
-app.get('/pop', function(req, res) {
-  console.log("Pop!");
-  client.watch(tube).onSuccess(function(data) {
-    client.reserve().onSuccess(function(job) {
-      res.send(job.data);
-    });
-  });
-});
-
-app.get('/push', function(req, res){
-  console.log("Push!");
-  client.use(tube).onSuccess(function(data) {
-    console.log(data);
-
-    client.put('my job').onSuccess(function(data) {
-      console.log(data);
-      client.disconnect();
-    });
   });
 });
 
